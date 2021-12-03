5ea9e930889c9b22c129366eeb86ebbc8edc5c0f
Felipe dos Anjos <felipe.b.abencoado@gmail.com>
2021-12-03 16:40:50 -0300

    feat: add login oauth github

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import { Header } from 'src/components/Header';
 import { AppProps } from 'next/app';
+import { SessionProvider as NextAuthProvider } from 'next-auth/react';
 import 'src/styles/global.scss';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <>
-      <Header />
-      <Component {...pageProps} />
+      <NextAuthProvider session={session}>
+        <Header />
+        <Component {...pageProps} />
+      </NextAuthProvider>
     </>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,31 @@
+import { GetStaticProps } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import { SubscribeButton } from "src/components/SubscribeButton";
+import { stripe } from "src/services/strype";
 
 import Woman from "public/images/woman.svg";
 
 import styles from "./home.module.scss";
 
-export default function Home() {
+/*
+ * Formas de fazer chamadas a Api no next:
+ * Client side;
+ * Server-side - permite indexação e popular a página com informações dinâmicas diferentes para cada usuário;
+ * Static site generation - usado quando html é igual para todos que acessam a página;
+ *
+ */
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
+
+
   return (
     <>
       <Head> 
@@ -18,9 +37,9 @@ export default function Home() {
           <h1>News about the <span>React</span> world</h1>
           <p>
             Get access to all publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <Image
@@ -31,3 +50,42 @@ export default function Home() {
     </>
   )
 }
+
+export const getStaticProps:GetStaticProps = async () => { 
+  
+  const price = await stripe.prices.retrieve('price_1K077lCreH6bI1TwvHXzMJ3m')
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format( price.unit_amount / 100),
+  }
+
+  return {
+    props: {
+      product
+    },
+    revalidate: 60 * 60 * 24 //24 hours
+  }
+}
+
+// export const getServerSideProps:GetServerSideProps = async () => { 
+  
+//   const price = await stripe.prices.retrieve('price_1K077lCreH6bI1TwvHXzMJ3m')
+
+//   const product = {
+//     priceId: price.id,
+//     amount: new Intl.NumberFormat('en-US', {
+//       style: 'currency',
+//       currency: 'USD',
+//     }).format( price.unit_amount / 100),
+//   }
+
+//   return {
+//     props: {
+//       product
+//     }
+//   }
+// }
